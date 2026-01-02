3ac507d3a3ac2e54c15989b39132511ce9890e8b
King-Perb <legendarycosmicdisc@gmail.com>
2026-01-02 18:29:29 +0000

    refactor(eslint): migrate to flat config format
    - Replace .eslintrc.js with eslint.config.mjs for mobile and shared packages
    - Update mobile package to use eslint-config-expo/flat.js
    - Update shared package with TypeScript ESLint flat config
    - Update package.json files to reference new config files

--- packages/mobile/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ['expo', 'prettier'],
-    rules: {
-        // Disable rules that conflict with Expo Router conventions
-        'import/no-unresolved': 'off',
-    },
-};
---
+++ packages/mobile/eslint.config.mjs
@@ -0,0 +1,13 @@
+import expo from 'eslint-config-expo/flat.js';
+
+export default [
+    ...expo,
+    {
+        ignores: [
+            'node_modules/**',
+            '.expo/**',
+            'dist/**',
+            'build/**',
+        ],
+    },
+];
